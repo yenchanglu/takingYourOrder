3a964554355cfbc21c46fc65ccf7b87347aad396
leonlu <yenchanglu@gmail.com>
2021-01-04 07:56:54 +0800

    fix typo

--- unit_test.py
+++ unit_test.py
@@ -4,7 +4,7 @@ import _FaceRecog
 class test_eigenface(unittest.TestCase):
 	def test_result(self):
             accuracy = _FaceRecog.validation()
-            pritn('Accuracy: ' + str(accuracy * 100) + '%')
+            print('Accuracy: ' + str(accuracy * 100) + '%')
             self.assertGreaterEqual(accuracy, 0.8)
 if __name__ == "__main__":
 	unittest.main()
